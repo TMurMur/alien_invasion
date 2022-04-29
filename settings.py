9decf0ead827b99bf9dbd2fd0f43a4936c1a6a06
TMurMur <8060750@gmail.com>
2022-04-29 21:12:57 +0300

    screen, ship, speed and borders, q-for quit

--- settings.py
+++ settings.py
@@ -6,4 +6,8 @@ class Settings():
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (230, 230, 230)
+        # self.bg_color = (230, 230, 230) # grey screen background
+        self.bg_color = (70, 140, 70) # dark-green screen background
+        
+        # Ship settings
+        self.ship_speed = 1.5
