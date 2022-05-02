3044131399f6b887a578dde33a7504781026803c
TMurMur <8060750@gmail.com>
2022-05-02 20:45:02 +0300

    fleet, bullets, movements

--- settings.py
+++ settings.py
@@ -3,11 +3,26 @@ class Settings():
 
     def __init__(self):
         """Initialize the game's settings."""
+        
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
         # self.bg_color = (230, 230, 230) # grey screen background
-        self.bg_color = (70, 140, 70) # dark-green screen background
+        # self.bg_color = (70, 140, 70) # dark-green screen background
+        self.bg_color = (48, 20, 58) # dark-violet screen background
         
+        # Bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (200, 0, 14)
+        self.bullets_allowed = 3
+        
+        # Alien settings
+        self.alien_speed = 0.5
+        self.fleet_drop_speed = 6
+        # fleet_direction of 1 represents right: -1 represents left.
+        self.fleet_direction = 1
+
         # Ship settings
         self.ship_speed = 1.5
